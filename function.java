70d6a5d239e16707d1973a1987fdba5a02dce3c7
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-08-25 23:25:41 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,85 @@
+// Q.Print hello world
+// public class function{
+//     public static void  Hello(){ // function hai 
+//         System.out.println("Arman");
+       
+//     }
+  
+
+   
+//     public static void main(String args[]){
+//         Hello(); // func ko call kiye hai
+//     }
+// }
+
+
+// public class function{
+//     public static int  Hello(){ // yha void nhi int hia to isme return bhi krana hoga niche 
+//         System.out.println("Arman");
+//         System.out.println("Arman");
+//         System.out.println("Arman");
+
+//         return 3; // 3times out print hoga to return3 
+        
+       
+//     }
+  
+
+   
+//     public static void main(String args[]){
+//         Hello(); // func ko call kiye hai
+//     }
+// }
+
+
+
+// Q. print hello world
+// public class function{
+//     public static int  Hello(){ // yha void nhi int hia to isme return bhi krana hoga niche 
+//         System.out.println("Arman");
+//         System.out.println("Arman");
+//         System.out.println("Arman");
+
+//         return 3; // 3times out print hoga to return3 
+        
+       
+//     }
+  
+
+   
+//     public static void main(String args[]){
+//         Hello(); // func ko call kiye hai
+//     }
+// }
+
+
+
+
+
+// Q1. add two number using function(parameters)
+// import java.util.*;
+// public class function{
+//     public static void calculateSum( int num1, int num2){  // yha num1, num2 ko parameters or formal parameters kehte  hai
+//         int sum = num1 + num2;
+//         System.out.println("The sum is: " +sum); 
+//     }
+
+//     public static void main(String args[]){
+//      Scanner sc = new Scanner(System.in);
+//      System.out.print("Enter the first Number:");
+//      int a = sc.nextInt();
+//      System.out.print("Enter the first Number:");
+
+//      int b = sc.nextInt();
+//      calculateSum(a,b); // func ko call kiye hai ( yha a or b ko Arguments or Actual parameters kehte hai)
+//     }
+// }
+
+
+// Q Find the product of A ans B
+
+
+
+
+
+
